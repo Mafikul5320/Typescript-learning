fdc9023584a39bd2c510787c90255121173f085e
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-12-06 23:35:34 +0600

    Object , Literal & Optional Type

---
+++ src/nonPrimited.ts
@@ -0,0 +1,20 @@
+// array
+
+let name: [string, number] = ["mafikul", 20];
+
+
+
+const TodayList : (string | number)[] =  ['Alu', 20, "begon",500];
+
+const company:{
+    name: string,
+    Age: number,
+    Location: string,
+    isSucess: boolean,
+
+} ={
+    name: "ABC",
+    Age: 20,
+    Location: "Dhaka",
+    isSucess :true
+};
