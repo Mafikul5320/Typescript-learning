04771a45235a3eda9445a6e03a61ffc9db614fda
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-12-20 23:19:05 +0600

    update test file

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,41 @@
 const course : string ="Next level Web Development" ;
-console.log(course)
+console.log(course);
+
+type RichPeople = {
+    name: string;
+    age: number;
+    car: string;
+}
+
+type people1 = "name" | "age" | "car";
+type people2 = keyof RichPeople;
+
+const BestPeople: people2 = "age";
+
+type user = {
+    id: number;
+    name: string;
+    address: {
+        district: string;
+        sub_district: string
+    };
+    isActive: boolean;
+
+};
+
+const user1: user = {
+    name: "Rohim",
+    id: 22,
+    address: {
+        district: "AB",
+        sub_district: "BC"
+    },
+    isActive: true
+}
+
+const findUser = <x>(obj: x, key: keyof x) => {
+
+    return obj[key];
+};
+const result = findUser(user1, "address");
+console.log(result)
